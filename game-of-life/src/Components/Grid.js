e1301ca656ee097d67236cbc77620a3d9cbab325
Angela Williams <liteworkr@Angelas-MacBook-Air.local>
2020-08-01 19:15:56 -0700

    Added some styling

--- game-of-life/src/Components/Grid.js
+++ game-of-life/src/Components/Grid.js
@@ -5,156 +5,157 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from 'reactstrap';
 
 function useInterval(callback, delay) {
-    const savedCallback = useRef();
-  
-    useEffect(() => {
-      savedCallback.current = callback;
-    }, [callback]);
-  
-    useEffect(() => {
-      function tick() {
-        savedCallback.current();
-      }
-      if (delay !== null) {
-        let id = setInterval(tick, delay);
-        return () => clearInterval(id);
-      }
-    }, [delay]);
-  }
+ const savedCallback = useRef();
+ 
+ useEffect(() => {
+ savedCallback.current = callback;
+ }, [callback]);
+ 
+ useEffect(() => {
+ function tick() {
+ savedCallback.current();
+ }
+ if (delay !== null) {
+ let id = setInterval(tick, delay);
+ return () => clearInterval(id);
+ }
+ }, [delay]);
+ }
 
 function Grid() {
-    // react hook
-    const [gridSize, setGridSize] = useState(20)
-    const [grid, setGrid] = useState(createGrid(gridSize, 9))
-    const [auto, setAuto] = useState(null)
-    const [canAutoPlay, setCanAutoPlay ] = useState(false)
-    const [counter, setCounter ] = useState(0)
-    function createGrid(size, liveCellRow) {
-        // arr is array of rows
-        var arr = []
-        // creating rows
-        for (var y = 0; y < size; y++) {
-            var row = []
-            // creating cells
-            for (var x = 0; x < size; x++) {
-                if (y == liveCellRow) {
-                    row.push(true)
-                } else {
-                row.push(false)
-                }
-        
-            }
-            // creates grid of cells
-            arr.push(row)
-        }
-        return arr
-    }
+ // react hook
+ const [gridSize, setGridSize] = useState(20)
+ const [grid, setGrid] = useState(createGrid(gridSize, -1))
+ const [auto, setAuto] = useState(null)
+ const [canAutoPlay, setCanAutoPlay ] = useState(false)
+ const [counter, setCounter ] = useState(0)
+ function createGrid(size, liveCellRow) {
+ // arr is array of rows
+ var arr = []
+ // creating rows
+ for (var y = 0; y < size; y++) {
+ var row = []
+ // creating cells
+ for (var x = 0; x < size; x++) {
+ if (y == liveCellRow) {
+ row.push(true)
+ } else {
+ row.push(false)
+ }
+ 
+ }
+ // creates grid of cells
+ arr.push(row)
+ }
+ return arr
+ }
 
-    function resetGrid() {
-        setGrid(createGrid(gridSize))
-        setCounter(0)
-    }
+ function resetGrid() {
+ setGrid(createGrid(gridSize))
+ setCounter(0)
+ }
 
-    function newGrid() {
-        var liveCells = 0
-        setCounter(counter + 1)
-        var newGrid = grid.map( (row, y) => {
-            return row.map ( (cell, x) => {
-                var n =  countNeighbors(grid, x, y)
-                if (cell == true) {
-                    liveCells = liveCells + 1
-                }
-                // logic that determines if a cell lives or dies
-                if (n>=4){return false}
-                else if (n<=1){return false}
-                else if (n === 3){return true}
-                else if (n === 2 && cell==true){return true}
-            })
-          })
-          if (liveCells == 0) {
-              setCanAutoPlay(false)
-          }
-          setGrid(newGrid)
-    }
+ function newGrid() {
+ var liveCells = 0
+ setCounter(counter + 1)
+ var newGrid = grid.map( (row, y) => {
+ return row.map ( (cell, x) => {
+ var n = countNeighbors(grid, x, y)
+ if (cell == true) {
+ liveCells = liveCells + 1
+ }
+ // logic that determines if a cell lives or dies
+ if (n>=4){return false}
+ else if (n<=1){return false}
+ else if (n === 3){return true}
+ else if (n === 2 && cell==true){return true}
+ })
+ })
+ if (liveCells == 0) {
+ setCanAutoPlay(false)
+ }
+ setGrid(newGrid)
+ }
 
-    function autoPlay() {
-        setCanAutoPlay(true)
-    }
+ function autoPlay() {
+ setCanAutoPlay(true)
+ }
 
-    useInterval(() => {
-        if(canAutoPlay) {
-            newGrid();
-        }
-      }, 1000);
+ useInterval(() => {
+ if(canAutoPlay) {
+ newGrid();
+ }
+ }, 1000);
 
-    function autoStop() {
-        setCanAutoPlay(false)
-    }
+ function autoStop() {
+ setCanAutoPlay(false)
+ }
 
-    function toggleCell(x,y) {
-        var newGrid = [...grid]
-        newGrid [y][x] = !newGrid [y][x]
-        setGrid(newGrid)
-    }
+ function toggleCell(x,y) {
+ var newGrid = [...grid]
+ newGrid [y][x] = !newGrid [y][x]
+ setGrid(newGrid)
+ }
 
-    function countNeighbors(grid, x, y) {
-        var vals = [-1, 0, 1]
-        var count = 0 
-        vals.map((a) => {
-            vals.map((b) => {
-                // checking if negative 1 exists
-            if (grid[y+a] && grid[y+a][x+b]) {
-                if ((a == 0) && (b == 0)) {
+ function countNeighbors(grid, x, y) {
+ var vals = [-1, 0, 1]
+ var count = 0 
+ vals.map((a) => {
+ vals.map((b) => {
+ // checking if negative 1 exists
+ if (grid[y+a] && grid[y+a][x+b]) {
+ if ((a == 0) && (b == 0)) {
 
-                } else {
-                    count++
-                }
-            }
-            })
-        })
-        return count
-    }
+ } else {
+ count++
+ }
+ }
+ })
+ })
+ return count
+ }
 
-    
+ 
 
-    return (
-      <div> 
-      <div className='grid'>
-          {
-              grid.map( (row, y) => {
-                return row.map ( (cell, x) => {
-                    return(
-                        <div onClick={() => {toggleCell(x,y)}}key={`${x}${y}`} className='cell' style={{
-                            // makes so no matter how many cells it will be correct height/width
-                            height: `${100/row.length}%`, width: `${100/row.length}%`, backgroundImage: cell ? "url('https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/33/1817.png')": "url('https://rondexter.com/intermediate/equipment/focus_pattern.gif')"
-                            // backgroundColor: cell ? "violet":"white"
-                            // ternary operator
-                        }}>
-                        </div>
-                    )
-                })
-              })
-          }
-      </div>
-          <p className='gen'>Generation No: {counter}</p>
-    <div className='control-buttons'>
-    {/* <button onClick={newGrid}>Play  <FontAwesomeIcon icon={faPlay} /></button> */}
-    <Button onClick={newGrid} color="success">PLAY 1 MOVE <FontAwesomeIcon icon={faPlay} /></Button>
-    <Button onClick={autoPlay} color="success">PLAY <FontAwesomeIcon icon={faPlay} /></Button>
-    {/* <button>Stop  <FontAwesomeIcon icon={faStop} /></button> */}
-    <Button onClick={autoStop} color="danger">STOP <FontAwesomeIcon icon={faStop} /></Button>
-    {/* <button onClick={resetGrid}>Clear </button> */}
-    {/* <button>Random </button> */}
-    {/* <Button color="warning">RANDOM </Button> */}
-    {/* <Button color="warning">CHANGE SPEED </Button> */}
-    <Button onClick={resetGrid} color="info">CLEAR </Button>
-    <input onKeyUp = {(e) => {setGridSize(e.target.value); console.log(gridSize)}} />
-    <Button onClick ={() => {
-        setGrid(createGrid(gridSize))
-    }}>Change Size</Button>
-    </div>
-      </div>
-    )
-  }
+ return (
+ <div> 
+ <div className='grid'>
+ {
+ grid.map( (row, y) => {
+ return row.map ( (cell, x) => {
+ return(
+ <div onClick={() => {toggleCell(x,y)}}key={`${x}${y}`} className='cell' style={{
+ // makes so no matter how many cells it will be correct height/width
+ height: `${100/row.length}%`, width: `${100/row.length}%`, backgroundImage: cell ? "url('https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/33/1817.png')": "url('https://rondexter.com/intermediate/equipment/focus_pattern.gif')"
+ // backgroundColor: cell ? "violet":"white"
+ // ternary operator
+ }}>
+ </div>
+ )
+ })
+ })
+ }
+ </div>
+ <p className='gen'>Generation No: {counter}</p>
+ <div className='control-buttons'>
+ {/* <button onClick={newGrid}>Play <FontAwesomeIcon icon={faPlay} /></button> */}
+ <Button onClick={newGrid} color="success">PLAY 1 MOVE <FontAwesomeIcon icon={faPlay} /></Button>
+ <Button onClick={autoPlay} color="success">PLAY <FontAwesomeIcon icon={faPlay} /></Button>
+ {/* <button>Stop <FontAwesomeIcon icon={faStop} /></button> */}
+ <Button onClick={autoStop} color="danger">STOP <FontAwesomeIcon icon={faStop} /></Button>
+ {/* <button onClick={resetGrid}>Clear </button> */}
+ {/* <button>Random </button> */}
+ {/* <Button color="warning">RANDOM </Button> */}
+ {/* <Button color="warning">CHANGE SPEED </Button> */}
+ <Button onClick={resetGrid} color="info">CLEAR </Button>
+ <p className="cellSize">Cell Size:</p>
+ <input className="input" onKeyUp = {(e) => {setGridSize(e.target.value); console.log(gridSize)}} />
+ <Button color="warning" onClick ={() => {
+ setGrid(createGrid(gridSize))
+ }}>CHANGE SIZE</Button>
+ </div>
+ </div>
+ )
+ }
 
 export default Grid
--- game-of-life/src/Components/Rules.js
+++ game-of-life/src/Components/Rules.js
@@ -1,61 +1,32 @@
 import React, {useState} from "react";
 import '../App.css'
-import { Jumbotron, Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
+import { Jumbotron, Button, Collapse, CardBody, Card} from 'reactstrap';
 
 function Rules() {
-  const [popoverOpen, setPopoverOpen] = useState(false);
+ const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => setPopoverOpen(!popoverOpen);
-    return (
-      // <div>
-      //   {/* if cell has 4 or more active neighbors it dies or if it has 1 or less, also dies */}
-      //     {/* <h1>Welcome to John Conway's "Game of Life"!</h1> */}
-      //     <p>This is a computer science classic from 1970, a program that simulates a cellular automaton (plural automata). It has connections to all kinds of different aspects of computer science and nature.</p>
-      //     <ul>Game Rules</ul>
-      //     <li>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</li>
-      //     <li>Any live cell with two or three live neighbours lives on to the next generation.</li>
-      //     <li>Any live cell with more than three live neighbours dies, as if by overpopulation.</li>
-      //     <li>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
-      // </div>
-      <div>
-      <Jumbotron>
-        <h1 className="display-3">John Conway's Game of Life</h1>
-        {/* <p className="lead">This is a computer science classic from 1970, a program that simulates a cellular automaton (plural automata). It has connections to all kinds of different aspects of computer science and nature.</p> */}
-        {/* <hr className="my-2" /> */}
-        {/* <p>It uses utility classes for typography and spacing to space content out within the larger container.</p> */}
-        {/* <p className="lead"> */}
-        {/* <Button id="Popover1" type="button">
-        Launch Popover
-      </Button>
-      <Popover placement="bottom" isOpen={popoverOpen} target="Popover1" toggle={toggle}>
-        <PopoverHeader>Popover Title</PopoverHeader>
-        <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
-      </Popover>
-      <Button id="Popover1" type="button">
-        Launch Popover
-      </Button>
-      <Popover placement="bottom" isOpen={popoverOpen} target="Popover1" toggle={toggle}>
-        <PopoverHeader>Popover Title</PopoverHeader>
-        <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
-      </Popover>
-      <Button id="Popover1" type="button">
-        Launch Popover
-      </Button>
-      <Popover placement="bottom" isOpen={popoverOpen} target="Popover1" toggle={toggle}>
-        <PopoverHeader>Popover Title</PopoverHeader>
-        <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
-      </Popover>
-      <Button id="Popover1" type="button">
-        Launch Popover
-      </Button>
-      <Popover placement="bottom" isOpen={popoverOpen} target="Popover1" toggle={toggle}>
-        <PopoverHeader>Popover Title</PopoverHeader>
-        <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
-      </Popover> */}
-        {/* </p> */}
-      </Jumbotron>
-    </div>
-    )
-  }
+ const toggle = () => setIsOpen(!isOpen);
 
+
+ return (
+
+ <div>
+ {/* <Jumbotron>
+ <h1 className="display-3">John Conway's Game of Life</h1>
+ </Jumbotron> */}
+ <h1 className="header">John Conway's Game of Life</h1>
+ <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>RULES</Button>
+ <Collapse isOpen={isOpen}>
+ <Card>
+ <CardBody>
+ <ul></ul>
+ <li>Any live cell with two or three live neighbours survives.</li>
+<li>Any dead cell with three live neighbours becomes a live cell.</li>
+<li>All other live cells die in the next generation. Similarly, all other dead cells stay dead.</li>
+ </CardBody>
+ </Card>
+ </Collapse>
+ </div>
+ )
+ }
 export default Rules
